f3b8499215a4238a196f19c09d5c544f7345d594
nagano <michel.nagano@gmail.com>
2018-06-30 01:37:21 -0300

    Allow data to be passed through the 'props' attribute '=' only works for global variable, for now.

--- reusable.js
+++ reusable.js
@@ -1,19 +1,42 @@
 (function(global){
     const document = global.document;
 
-    function convertHTMLObjectToArray(HTMLObject){
+    function convertHTMLObjectToArray(componentName){
+        let HTMLObject = document.getElementsByTagName(componentName)
         return [].slice.call(HTMLObject);
     }
 
     const Reusable = {
-        parseTemplate: function(config){
+        parseTemplate: function(element, config){
             let template = config.template;
+            let props = config.props;
+            let data = config.data;
+
+            function replaceVarInTemplate(variable){
+                if(data && variable in data){
+                    return data[variable];
+                }
+
+                if(props){
+                    let attributeValue = element.attributes[variable].value;
+                    switch(props[variable]){
+                        case '@':
+                            return attributeValue;
+                            break;
+                        case '=':
+                            return global[attributeValue];
+                            break;
+                        default:
+                            console.log('none');
+                    }
+                }
+            }
+
             const expressions = config.template.match(/{{\s+\w+\s+}}/gi);
 
             expressions.forEach(function(expression){
                 let variable = expression.match(/(\w+)/)[0];
-
-                template = template.replace(new RegExp(expression), config.data[variable]);
+                template = template.replace(new RegExp(expression), replaceVarInTemplate(variable));
             });
 
             return template;
@@ -28,10 +51,10 @@
          */
         component: function(componentName, config){
             document.createElement(componentName);
-            const componentInstances = convertHTMLObjectToArray(document.getElementsByTagName(componentName));
+            const componentInstances = convertHTMLObjectToArray(componentName);
 
             componentInstances.forEach(element => {
-                element.innerHTML = this.parseTemplate(config);
+                element.innerHTML = this.parseTemplate(element, config);
             });
         }
     }
