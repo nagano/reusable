a81380008105f7da8e9d11a68a5224a31fed01cf
nagano <michel.nagano@gmail.com>
2018-06-30 00:35:36 -0300

    Add initial templating using mustache notation

---
+++ reusable.js
@@ -0,0 +1,39 @@
+(function(global){
+    const document = global.document;
+
+    function convertHTMLObjectToArray(HTMLObject){
+        return [].slice.call(HTMLObject);
+    }
+
+    const Reusable = {
+        parseTemplate: function(config){
+            let template = config.template;
+            const expressions = config.template.match(/{{\s+\w+\s+}}/gi);
+
+            expressions.forEach(function(expression){
+                let variable = expression.match(/(\w+)/)[0];
+
+                template = template.replace(new RegExp(expression), config.data[variable]);
+            });
+
+            return template;
+        },
+
+        /**
+         * config: {
+         *  data <object>
+         *  template <string>
+         * }
+         */
+        registerComponent: function(componentName, config){
+            document.createElement(componentName);
+            const componentInstances = convertHTMLObjectToArray(document.getElementsByTagName(componentName));
+
+            componentInstances.forEach(element => {
+                element.innerHTML = this.parseTemplate(config);
+            });
+        }
+    }
+
+    global.Reusable = Reusable;
+})(window);
